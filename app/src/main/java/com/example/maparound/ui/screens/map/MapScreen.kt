cb0b8e5b78ac7c6896d749aaadf5a992b77089e5
Changhao Wu <changhaowu00@gmail.com>
2023-05-14 10:19:35 +0200

    Added Map Dark an light Styles

--- app/src/main/java/com/example/maparound/ui/screens/map/MapScreen.kt
+++ app/src/main/java/com/example/maparound/ui/screens/map/MapScreen.kt
@@ -1,25 +1,43 @@
 package com.example.maparound.ui.screens.map
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.maparound.R
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.maps.android.compose.*
 
 @Composable
 fun MapScreen(){
+
+    val context = LocalContext.current
+
+    val theme = if (isSystemInDarkTheme()) R.raw.map_style_dark else R.raw.map_style_light
+
+    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+    var properties by remember {
+        mutableStateOf(MapProperties(
+            mapType = MapType.NORMAL,//MapType.SATELLITE,
+            mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context, theme)
+        ))
+    }
+
     val singapore = LatLng(1.35, 103.87)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
+        cameraPositionState = cameraPositionState,
+        properties = properties,
+        uiSettings = uiSettings
+
+
     ) {
         Marker(
             state = MarkerState(position = singapore),
