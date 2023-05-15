7c7577660c341c1b5233772d38d60277bb2ed1a7
Changhao Wu <changhaowu00@gmail.com>
2023-05-15 23:57:50 +0200

    Upgraded grandle dependecy : Compose, material3, kotlin version, daggerhilt

--- app/src/main/java/com/example/maparound/ui/screens/map/MapScreen.kt
+++ app/src/main/java/com/example/maparound/ui/screens/map/MapScreen.kt
@@ -17,11 +17,13 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -167,7 +169,11 @@ fun MapScreen(
         date_time = "12/04/23, 12:00",
         publish_time = "23d"
     )
-    Box(modifier = Modifier.padding(top = 300.dp)){
+    Box(
+        modifier = Modifier.padding(top = 300.dp).clip(
+            RoundedCornerShape(10.dp))
+
+    ){
         HomeListItem(place = place1)
     }
 
